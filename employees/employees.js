3772406c2bddb316908ee1a705af3561e0921d41
dh0728 <dustkscjswo@naver.com>
2024-02-21 17:33:24 +0900

    login_DH

--- employees/employees.js
+++ employees/employees.js
@@ -7,16 +7,16 @@ const CusManage = require('../cusManage/cusManage');
 
 
 async function employees(){
-  console.log('1.매출확인 2.메뉴확인 3.재고관리 4.고객관리 5.뒤로가기 6.종료');
+  console.log('1.매출확인 2.메뉴관리 3.재고관리 4.고객관리 5.뒤로가기 6.종료');
   let select = await Input.getUserInput();
     if (select === '1') {
-      SalesCheck.salesCheck();
+      await SalesCheck.salesCheck();
     }else if (select === '2'){
-      MenuCheck.menuCheck();
+      await MenuCheck.menuCheck();
     }else if (select === '3'){
-      StockCheck.stockCheck();
+      await StockCheck.stockCheck();
     }else if (select === '4'){
-      CusManage.cusManage();
+      await CusManage.cusManage();
     }else if (select === '5'){
       return ;
     }else if (select === '6'){
--- menu/menu.js
+++ menu/menu.js
@@ -1,8 +1,10 @@
 const Input = require('../userInput'); 
 
 async function menu(){
-
+  await sortdocs(client, "mydb", "products");
 }
+
+async function sortdocs(client, dbname, colname){}
 
 
 module.exports = {menu};
