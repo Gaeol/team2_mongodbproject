04ceb21ae3ce87740d4fd4b1b693016a34b8fd29
eggzuxi <eggzuxi99@gmail.com>
2024-02-22 16:01:59 +0900

    forpull_pjh

--- delete.js
+++ delete.js
@@ -16,7 +16,13 @@ async function deleteMenu(client, mongoCafe, Menu){
   console.log("메뉴가 삭제되었습니다");
 };
 
-// async function 
+async function cancelOrder(client, mongoCafe, Orders){
+  console.log("삭제할 메뉴의 이름을 입력하세요")
+  const delMenu = await getUserInput()
+  let myqry = { "name": `${delMenu}` };
+  const result = await client.db(mongoCafe).collection(Menu).deleteOne(myqry);
+  console.log("메뉴가 삭제되었습니다");
+};
 
 module.exports = {deleteAccount};
 module.exports = {deleteMenu};
