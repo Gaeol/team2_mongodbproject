d55de4d4db41f295da34d7bde5f4af30fd32d047
unknown <dsmamkm@gmail.com>
2024-02-22 15:55:28 +0900

    orders_주문하기_추가주문_min

--- menu/menu.js
+++ menu/menu.js
@@ -1,10 +1,9 @@
 const Input = require('../userInput'); 
 
-
-
 async function menu(client, dbname, colname){
   const result = await client.db(dbname).collection(colname).find({}).toArray();
   console.table(result);
 };
+
 
 module.exports = {menu};
