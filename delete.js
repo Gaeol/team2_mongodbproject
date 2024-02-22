2ad30cba0ce3249ba7ebd01a62cc14fbe567aab5
dh0728 <dustkscjswo@naver.com>
2024-02-23 02:45:39 +0900

    로그인_회원가입중복검사_DH

--- delete.js
+++ delete.js
@@ -1,9 +1,9 @@
 const { getUserInput } = require('./userInput');
 
-async function deleteAccount(client, mongoCafe, Customers){
-  console.log("삭제할 회원의 이름을 입력하세요")
-  const delName = await getUserInput()
-  let myqry = { "name": `${delName}` };
+async function deleteAccount(client, mongoCafe, Customers,user){
+  // console.log("삭제할 회원의 이름을 입력하세요")
+  // const delName = await getUserInput()
+  let myqry = { "name": `${user}` };
   const result = await client.db(mongoCafe).collection(Customers).deleteOne(myqry);
   console.log("계정이 삭제되었습니다");
 };
--- join/join.js
+++ join/join.js
@@ -1,11 +1,14 @@
 const Input = require('../userInput');
 const Insert = require('../insert');
+const Exam = require('../exam');
 
 async function registerUser(client) {
   console.log('이름을 입력해주세요: ');
   let username = await Input.getUserInput();
   console.log('아이디을 입력해주세요: ');
   let userid = await Input.getUserInput();
+  userid= await Exam.examId(client, "mongoCafe", "Customers", userid)
+  console.log(userid);
   console.log('비밀번호를 입력해주세요: ');
   let pwd = await Input.getUserInput();
   console.log('전화번호를 입력해주세요: ');
@@ -15,9 +18,12 @@ async function registerUser(client) {
   console.log('카드번호 16자리를 입력해주세요: ');
   let cardNumber = await Input.getUserInput();
 
-  await Insert.userInsert(client, "mongoCafe", "Menu", {
-    "customer_id":parseInt(userid), "birthDate":`${birthDate}`, "name":`${username}`, "cardNumber":`${cardNumber}`,"paymentPassword":`${pwd}`,"totalPayment":parseInt(0),"membershipLevel": "Bronze","phoneNumber":`${phoneNumber}`});
-  console.log('가입이 완료되었습니다')
+  const result = await client.db("mongoCafe").collection("Customers").find({}).sort({_id :-1}).toArray();
+  const cusNum= result[0]['customerNumber']+1;
+
+  await Insert.userInsert(client, "mongoCafe", "Customers", {
+    "customer_id":parseInt(userid), "birthDate":`${birthDate}`, "name":`${username}`, "cardNumber":`${cardNumber}`,"paymentPassword":`${pwd}`,"totalPayment":parseInt(0),"membershipLevel": "Bronze","phoneNumber":`${phoneNumber}`,"customerNumber":parseInt(cusNum) });
+  console.log('가입이 완료되었습니다');
 }
 
 module.exports = { registerUser };
